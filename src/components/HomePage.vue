bb7a74dc66699ac61cbc181367e24e41061c8307
Sehzad23 <sehaans87@gmail.com>
2025-09-08 17:50:06 +0530

    first commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,155 @@
+<template>
+  <div class="container">
+
+    <section class="section-1">
+
+      <div class="content">
+        <div class="content-part1">
+          <h1>Lorem ipsum dolor <br> sit amet.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam autem sunt minus voluptate asperiores
+            optio recusandae earum quis mollitia officiis ratione, placeat atque.</p>
+
+          <h5>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
+
+          <button>Start Now Here</button>
+        </div>
+        <div class="content-part2">
+          <div class="img">
+            <img src="@/assets/images/introImage.png" alt="">
+          </div>
+        </div>
+      </div>
+
+
+    </section>
+
+    <section class="section-2">
+
+      <div class="content">
+        <h2>Lorem ipsum dolor, sit amet <br> consectetur adipisicing elit.</h2>
+        <div class="boxes">
+          <div class="box">
+            <div class="stars">
+              <i class="ri-star-s-fill"></i>
+              <i class="ri-star-s-fill"></i>
+              <i class="ri-star-s-fill"></i>
+              <i class="ri-star-s-fill"></i>
+            </div>
+            <h5>Lorem ipsum</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, aut?</p>
+          </div>
+          <div class="box">
+            <div class="stars">
+              <i class="ri-star-s-fill"></i>
+              <i class="ri-star-s-fill"></i>
+              <i class="ri-star-s-fill"></i>
+              <i class="ri-star-s-fill"></i>
+            </div>
+            <h5>Lorem ipsum</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, aut?</p>
+          </div>
+          <div class="box">
+            <div class="stars">
+              <i class="ri-star-s-fill"></i>
+              <i class="ri-star-s-fill"></i>
+              <i class="ri-star-s-fill"></i>
+              <i class="ri-star-s-fill"></i>
+            </div>
+            <h5>Lorem ipsum</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, aut?</p>
+          </div>
+          <div class="box">
+            <div class="stars">
+              <i class="ri-star-s-fill"></i>
+              <i class="ri-star-s-fill"></i>
+              <i class="ri-star-s-fill"></i>
+              <i class="ri-star-s-fill"></i>
+            </div>
+            <h5>Lorem ipsum</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, aut?</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section-3">
+      <div class="content">
+
+        <div class="section-part1">
+          <div class="section-part1-top">
+            <h2>Be Enthusiastic</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae neque fugit mollitia aspernatur quibusdam
+              nulla animi commodi repellat rerum, ratione laborum porro enim a est adipisci repellendus magnam quod
+              modi.
+            </p>
+            <h5>Lorem ipsum dolor sit<span>"amet"</span></h5>
+          </div>
+          <div class="section-part1-bottom">
+            <div class="boxes">
+              <div class="box">
+
+                <h5>800</h5>
+                <p>Lorem ipsum dolor sit amet consectetur</p>
+              </div>
+              <div class="box">
+
+                <h5>18</h5>
+                <p>Lorem ipsum dolor sit amet consectetur</p>
+              </div>
+              <div class="box">
+
+                <h5>16</h5>
+                <p>Lorem ipsum dolor sit amet consectetur</p>
+              </div>
+
+            </div>
+
+
+          </div>
+        </div>
+        <div class="section-part2">
+          <video autoplay loop muted src="@/assets/images/pinReel.mp4"></video>
+        </div>
+      </div>
+    </section>
+
+    <section class="section-4">
+
+      <div class="content">
+        <h2>Lorem ipsum dolor sit amet.</h2>
+
+        <div class="container">
+          <div class="left">
+            <img src="@/assets/images/pin1-removebg-preview.png" alt="">
+            <h5>Lorem</h5>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur optio aliquam, numquam maiores ab
+              aut cupiditate rerum atque reprehenderit cum.</p>
+          </div>
+
+          <div class="right">
+            <div class="top-right">
+              <div class="box">
+                <img src="@/assets/images/pin2-removebg-preview.png" alt="">
+              </div>
+              <div class="box">
+                <img src="@/assets/images/pin3-removebg-preview.png" alt="">
+              </div>
+            </div>
+
+            <div class="bottom-right">
+              <img src="@/assets/images/pin4-removebg-preview.png" alt="">
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </section>
+
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
